d38bf2e3c33fb5bfeb4e39c17ca0224a0a611a52
Rainer Sousa <rainervitorrv@gmail.com>
2025-03-27 15:32:49 -0300

    Adicionado função de comando parar criar usuario

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {name} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user in the application';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+
+        // Crie o usuário no banco de dados
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info("Usuário '{$user->name}' criado com sucesso!");
+    }
+}
